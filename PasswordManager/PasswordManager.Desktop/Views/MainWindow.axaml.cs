54cf733e41c31bfa816dcb777e026533067742a4
Adrian Panaite <adrianpanaite95@gmail.com>
2025-06-20 19:15:48 +0300

    Add convert celsius to fahrenheit

--- PasswordManager/PasswordManager.Desktop/Views/MainWindow.axaml.cs
+++ PasswordManager/PasswordManager.Desktop/Views/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 
 namespace PasswordManager.Desktop.Views;
 
@@ -7,5 +8,32 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (double.TryParse(Celsius.Text, out var celsius))
+        {
+            var fahrenheit = celsius * (9d / 5d) + 32;
+            Fahrenheit.Text = fahrenheit.ToString("0.0");
+        }
+        else
+        {
+            Celsius.Text = "0";
+            Fahrenheit.Text = "0";
+        }
+    }
+
+    private void Celsius_OnChange(object? sender, TextChangedEventArgs e)
+    {
+        if (double.TryParse(Celsius.Text, out var celsius))
+        {
+            var fahrenheit = celsius * (9d / 5d) + 32;
+            Fahrenheit.Text = fahrenheit.ToString("0.0");
+        }
+        else
+        {
+            Fahrenheit.Text = "0";
+        }
     }
 }
